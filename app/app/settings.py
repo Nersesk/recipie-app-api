59952f07b5f0e27289d6a6a15ec557dab79041fd
Nerses <nerses.karapetyan@onetick.com>
2023-08-22 21:31:31 +0400

    user_Api

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core,'
+    'core',
     'rest_framework',
-    'drf-spectacular',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+    'user'
 
 ]
 
@@ -133,6 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "core.User"
 
-REST_FRAMEWORK ={
-    "DEFAULT_SCHEMA": 'drf.spectacular.openapi.AutoSchema'
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
